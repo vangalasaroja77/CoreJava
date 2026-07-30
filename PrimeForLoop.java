b0052cd7cc505b1a39fd1f69776f781848091645
Jananya Vangala <vangalasaroja77@gmail.com>
2026-07-30 22:33:28 +0530

    third push

---
+++ PrimeForLoop.java
@@ -0,0 +1,22 @@
+public class PrimeOrNot {
+
+    static void main() {
+        int n=25;
+        boolean primeornot=false;
+        for(int i=2;i<n/2;i++)
+        {
+            if(n%i==0)
+            {
+                primeornot=true;
+            }
+        }
+        if(primeornot)
+        {
+            System.out.println(n + " is not a prime number.");
+        }
+        else
+        {
+            System.out.println(n + " is a prime number.");
+        }
+    }
+}
